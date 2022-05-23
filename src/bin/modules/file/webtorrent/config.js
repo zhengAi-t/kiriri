ecf2ff08f58b1f2b7afdd4a4217dde9ad9442782
zhengAi <2857135591@qq.com>
2022-05-23 09:52:10 +0800

    push

--- src/bin/modules/file/webtorrent/config.js
+++ src/bin/modules/file/webtorrent/config.js
@@ -2,7 +2,7 @@ import  {addResource} from './webtorrent';
 //本文件为配置文件，下面的是默认的配置
 //需要配置这个模块是如何得到magnetURI的
 //这里的代码不属于引擎自身
-addResource('magnet:?xt=urn:btih:c53baf7f52c9382d8efce16a34898b2044c168a6&dn=game.pak&tr=wss%3A%2F%2Ftomodachi.top%3A19673&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com');
+addResource('magnet:?xt=urn:btih:c53baf7f52c9382d8efce16a34898b2044c168a6&dn=game.pak&tr=http%3A%2F%2Ftracker.ipv6tracker.ru%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.mywaifu.best%3A6969%2Fannounce&tr=http%3A%2F%2Fvps02.net.orel.ru%3A80%2Fannounce&tr=https%3A%2F%2Fcarbon-bonsai-621.appspot.com%3A443%2Fannounce&tr=https%3A%2F%2Ft.btcland.xyz%3A443%2Fannounce&tr=https%3A%2F%2Ft1.tokhmi.xyz%3A443%2Fannounce&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com');
 // import application from '../../../lib/buildup';
 // if(location){
 //   let match=location.href.match(/magnet:\?[a-zA-Z%=&\.\-:]+/);
